782dd20da1783794e8753a5675f0703798d59499
Gaurav Rajput <er.gaurav2022@gmail.com>
2025-02-08 12:08:51 +0530

    portfolio deploy

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings : ["a Frontend Developer" , "Excellent in DSA" ,"a Web Developer","Proficient in C++, JavaScript" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
